328de8b07fc4f733300e388bcdd8721a702b2d24
sergeybolzan <sergey.bolzan@gmail.com>
2018-02-26 21:23:07 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  for(let i = 5; number / i >= 1; i *= 5)
+  	result += number / i ^ 0;
+  return result;
 }
